6bfb8be69971aa445e3edab8531a0cb9ac28a0fd
Ahmad Amer Bakran <ahmbak01@easv365.dk>
2023-03-20 19:41:59 +0100

    initial GUI1

--- src/dal/database/DBConnector.java
+++ src/dal/database/DBConnector.java
@@ -15,8 +15,8 @@ public class DBConnector {
         dataSource = new SQLServerDataSource();
         dataSource.setServerName("10.176.111.31");
         dataSource.setDatabaseName("Ticketing_System");
-        dataSource.setUser("CSe22A_12"); // Type your given username
-        dataSource.setPassword("Hamada007"); // Type your given password
+        dataSource.setUser("*****"); // Type your given username
+        dataSource.setPassword("*****"); // Type your given password
         dataSource.setTrustServerCertificate(true);
         dataSource.setPortNumber(1433); // From school
 
